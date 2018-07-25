a86a5f71513cd1463987637458b19449a1303412
Lycoris <lycoris@Lycoris-mbp.local>
2018-07-25 12:29:19 +0800

    update scrooge version to 18.7.0, fix bugs

---
+++ src/main/java/io/lycoris/ScroogePlugin.java
@@ -0,0 +1,11 @@
+package io.lycoris;
+
+import org.gradle.api.Project;
+import org.gradle.api.Plugin;
+
+public class ScroogePlugin implements Plugin<Project> {
+    public void apply(Project project){
+        project.getTasks().create("compileScrooge",ScroogeCompileTask.class);
+
+    }
+}
